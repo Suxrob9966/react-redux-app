252d96b9eb5b481635acca5cdbef45a28967077f
Suxrob9966 <rabbimovich9966@gmail.com>
2022-01-28 08:34:28 -0600

    working with multiple state properties

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -5,6 +5,8 @@ const Counter = () => {
   const dispatch = useDispatch(); // dispatch becomes a function
 
   const counter = useSelector((state) => state.counter); // useSelector() hook needs a function with state parameter from which we then extract the value that we need
+  const show = useSelector((state) => state.showCounter);
+
   const incrementHandler = () => {
     dispatch({
       type: 'increment',
@@ -24,12 +26,14 @@ const Counter = () => {
     });
   };
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({ type: 'toggle' });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>increment</button>
         <button onClick={increaseHandler}>increment by 5</button>
